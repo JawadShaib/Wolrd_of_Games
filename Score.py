5c6991e313f67ebc8d8cf63ed96332843c3e037d
JawadShaib <shaibjawad@gmail.com>
2023-03-07 00:13:14 +0200

    world of games part 3

--- Score.py
+++ Score.py
@@ -5,9 +5,11 @@ import time
 
 def add_score(dif: int) -> None:
     """adding  score to the curretnt score points points file"""
-    with open(u.SCORES_FILE_NAME, "w+") as f:
+    with open(u.SCORES_FILE_NAME, "r") as f:
         current_points = f.read()
         if not (current_points):
             current_points = 0
-        current_points += dif * 3 + 5
-        f.write(str(current_points))
+        res = int(current_points)
+        res += dif * 3 + 5
+    with open(u.SCORES_FILE_NAME, "w") as f:
+        f.write(str(res))
